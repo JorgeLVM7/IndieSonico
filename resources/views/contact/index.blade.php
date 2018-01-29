5e417e6718bebdaca8028e6534af9ff1f9c7f682
JorgeLVM7 <jorgito11@gmail.com>
2018-01-29 11:04:57 -0600

    act6
    Formulario de contacto

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     {{--    CAROUSEL    --}}
-    <div id="carouselExampleSlidesOnly" class="carousel slide fixed-absolute" data-ride="carousel">
+ {{--   <div id="carouselExampleSlidesOnly" class="carousel slide fixed-absolute" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img class="d-block w-100 top-ten" src="images/42Firma.jpg" alt="First slide">
@@ -19,70 +19,7 @@
                 </div>
             @endforeach
         </div>
-    </div>
-
-    <br>
-    {{--    PUBLICACIONES   --}}
-    <div class="row ">
-        <div class="col-8 scroll">
-            <div class="">
-                @foreach($articles as $article)
-                    <div class="card-body">
-                        ENCABEZADO
-                        <h4 class="card-title">{{ $article ->head }}</h4>
-                        HORA
-                        <h6 class="card-subtitle mb-2 text-muted"> {{$article->updated_at}}</h6>
-                        FOTO
-                        <img class="card-img-top img-articles" src="images/{{$article->path}}" alt="Card image cap">
-                        DESCRIPCION
-                        <p class="card-subtitle mb-2 text-muted">{{ $article -> description }}</p>
-                        BOTONES DE COMPARTIR
-                        <button type="button" class="btn btn-info">Ver más</button>
-                        <button type="button" class="btn btn-primary">Twitter</button>
-                        <button type="button" class="btn btn-primary">Facebook</button>
-
-                    </div>
-                @endforeach
-                {!! $articles->render() !!}
-            </div>
-        </div>
-
-        {{--    PUBLICIDAD  --}}
-        <div class="col-4 ">
+    </div>--}}
 
 
-            <div class="row ">
-                <div class="col-12">
-                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloremque eos eum impedit ipsam natus neque perferendis placeat possimus provident quae reiciendis repellendus sequi totam ut vel, velit vitae voluptatum.</p>
-                </div>
-            </div>
-
-
-            <div class="row">
-                <div class="col-12">
-                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloremque eos eum impedit ipsam natus neque perferendis placeat possimus provident quae reiciendis repellendus sequi totam ut vel, velit vitae voluptatum.</p>
-                </div>
-            </div>
-
-
-            <div class="row">
-                <div class="col-12">
-                    <div class="col-6">
-                        <h3>SÍGUENOS: </h3>
-                    </div>
-                    <div class="col-6">
-                        <span><img src="icons/facebook.png" alt=""></span>
-                        <span><img src="icons/instagram.png" alt=""></span>
-                        <span><img src="icons/twitter.png" alt=""></span>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <p>¿TIENES UNA BANDA ?          <a class="siguenos" href="">ESCRÍBENOS</a>  </p>
-                    <p>SUBE TU FOTO/ NOTA /MÚSICA   <a class="siguenos" href="">COLABORA</a>    </p>
-                    <p>DUDA / SUGERENCIAS           <a class="siguenos" href="">CONTACTO</a>    </p>
-                </div>
-            </div>
-
-        </div>
-    </div>
 @endsection
